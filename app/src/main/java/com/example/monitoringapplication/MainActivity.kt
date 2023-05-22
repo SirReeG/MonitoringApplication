308738d265136ac37fef1f6dfc2010c916a55004
Ruslan <open99hearts@gmail.com>
2023-05-22 14:05:15 +0300

    Add netword  + api

--- app/src/main/java/com/example/monitoringapplication/MainActivity.kt
+++ app/src/main/java/com/example/monitoringapplication/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.monitoringapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.monitoringapplication.Api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDispostable = CompositeDisposable()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                       Log.d("TEST" ,it.toString())
+
+        },{
+
+        })
+
+
+        }
+    override fun onDestroy() {
+        super.onDestroy()
+
+        compositeDispostable.dispose()
     }
-}
+
+
+
+    }
+
 
